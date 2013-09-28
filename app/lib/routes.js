1dd12bfeb04f1b98d2db5955037173be0a21a300
jeffaudio <hentschel.jeff@gmail.com>
2013-09-28 01:10:10 -0400

    Added twitter and facebook auth routes.

--- app/lib/routes.js
+++ app/lib/routes.js
@@ -1,4 +1,6 @@
-module.exports = function (app) {
+var LOGIN_URL = "/login";
+
+module.exports = function (app, userController) {
 
 	// Return the index to the angular project.
 	function angularIndex(req, res, next) {
@@ -8,10 +10,23 @@ module.exports = function (app) {
 
 	app.get('/', angularIndex);
 
+	app.get('/user', userController.getCurrent)
+
+	// Facebook Authentication
+	app.get('/auth/facebook', passport.authenticate('facebook'), function(req, res) {});
+	app.get('/auth/facebook/callback', passport.authenticate('facebook', {failureRedirect: LOGIN_URL}), 
+		function(req, res) { res.redirect('/'); });
+
+
+
+
 	// Place other routes here.
 
 
-
+	function ensureAuthenticated(req, res, next) {
+	  if (req.isAuthenticated()) { return next(); }
+	  res.redirect(LOGIN_URL)
+	}
 	// Default action is to load the angular project.
 	app.get('*', angularIndex);
 };
